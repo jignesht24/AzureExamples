75cdffe3190372d0e1144082666395adf0a93a3f
Jignesh Trivedi <jignesht24@gmail.com>
2019-05-10 17:27:05 +0530

    init checkin

---
+++ AzureServiceBus-Queue/BasicExample/BasicReceiverQueueExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            string connectionStringServiceBus = "<<Namespace connection string>>";
+            string queueName = "<<queue name>>";
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press any key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            queueClient = new QueueClient(connectionStringServiceBus, queueName);
+
+            RegisterMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            queueClient.CloseAsync().Wait();
+        }
+        static void RegisterMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: Sequence Number:{message.SystemProperties.SequenceNumber} \t Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+        // Use this Handler to look at the exceptions received on the MessagePump
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Exception:: {exceptionReceivedEventArgs.Exception}.");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AzureServiceBus-Queue/BasicExample/BasicSenderQueueExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicSenderQueueExample
+{
+    class Program
+    {
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            string connectionStringServiceBus = "<<Namespace connection string>>";
+            string queueName = "<<queue name>>";
+
+            SendMessage(connectionStringServiceBus, queueName).GetAwaiter().GetResult();
+        }
+
+        static async Task SendMessage(string connectionStringServiceBus, string queueName)
+        {
+            const int numberOfMessages = 5;
+            queueClient = new QueueClient(connectionStringServiceBus, queueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press any key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send Messages
+            await SendMessagesToQueueAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesToQueueAsync(int numberOfMessages)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessages; i++)
+                {
+                    // Message that send to the queue
+                    string messageBody = $"GNR-MSG dataid:{i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    Console.WriteLine($"Sending message to queue: {messageBody}");
+
+                    // Send the message to the queue
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Exception: {exception.Message}");
+            }
+        }
+    }
+}
