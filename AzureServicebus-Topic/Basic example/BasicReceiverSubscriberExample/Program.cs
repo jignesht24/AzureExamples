ff08484d1104d3eb8d19d96cd31e99007b1ff3ab
Jignesh Trivedi <jignesht24@gmail.com>
2019-05-14 06:27:53 +0530

    initail checkedin

---
+++ AzureServicebus-Topic/Basic example/BasicReceiverSubscriberExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static ITopicClient topicClient;
+        static ISubscriptionClient subscriptionClient;
+        static void Main(string[] args)
+        {
+            string connectionStringServiceBus = "<<connection String Service Bus>>";
+            string topicName = "<<Topic Name>>";
+            string SubscriptionName = "<<Subscription Name>>";
+
+            subscriptionClient = new SubscriptionClient(connectionStringServiceBus, topicName, SubscriptionName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press any key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            topicClient = new TopicClient(connectionStringServiceBus, topicName);
+
+            RegisterMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            topicClient.CloseAsync().Wait();
+        }
+        static void RegisterMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: Sequence Number:{message.SystemProperties.SequenceNumber} \t Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+        // Use this Handler to look at the exceptions received on the MessagePump
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Exception:: {exceptionReceivedEventArgs.Exception}.");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AzureServicebus-Topic/Basic example/BasicSenderTopicExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicSenderQueueExample
+{
+    class Program
+    {
+        static ITopicClient topicClient;
+        static void Main(string[] args)
+        {
+            string connectionStringServiceBus = "<<connection String Service Bus>>";
+            string topicName = "<< Topic Name >>";
+
+            SendMessage(connectionStringServiceBus, topicName).GetAwaiter().GetResult();
+        }
+
+        static async Task SendMessage(string connectionStringServiceBus, string topicName)
+        {
+            const int numberOfMessages = 5;
+            topicClient = new TopicClient(connectionStringServiceBus, topicName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press any key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send Messages
+            await SendMessagesToQueueAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+        }
+
+        static async Task SendMessagesToQueueAsync(int numberOfMessages)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessages; i++)
+                {
+                    // Message that send to the queue
+                    string messageBody = $"GNR-MSG dataid:{i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    Console.WriteLine($"Sending message to queue: {messageBody}");
+
+                    // Send the message to the queue
+                    await topicClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Exception: {exception.Message}");
+            }
+        }
+    }
+}
